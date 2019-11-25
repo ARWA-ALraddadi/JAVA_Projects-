b27e7293346ddb1cb6db2fe6d96a6866c15fefae
ARWA <52074173+ARWA-ALraddadi@users.noreply.github.com>
2019-11-25 19:14:43 +1100

    Add files via upload

---
+++ calling_class/src/hom/work/HomWork.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hom.work;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class HomWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Professor  o1 = new Professor ( ) ;  
+         Department o2 = new Department () ; 
+        University o3 = new University () ; 
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ calling_class/src/hom/work/University.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hom.work;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class University {
+    Department f = new Department () ; 
+    int num1 ; 
+     University(){
+     num1 = f.num ; 
+     System.out.println( "the methed   University " + num1);
+     
+     }
+}
